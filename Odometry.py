3a1c3a1b86b8818a2b86703182d09b8a4813c768
Aidan Carrier <arcarrier01@gmail.com>
2026-04-10 00:49:25 -0400

    fixing error hopefully

--- Odometry.py
+++ Odometry.py
@@ -221,9 +221,9 @@ class Odometry(object):
         # print(f"accels: {self.__acceleration}")
         #When the robot is still, the accel values are near 0. In this case, set accel values to zero.
         
-        self.__acceleration = (self.__acceleration[0] - self.__accelerometer_offset[0],
-                               self.__acceleration[1] - self.__accelerometer_offset[1],
-                               self.__acceleration[2] - self.__accelerometer_offset[2])
+        # self.__acceleration = (self.__acceleration[0] - self.__accelerometer_offset[0],
+        #                        self.__acceleration[1] - self.__accelerometer_offset[1],
+        #                        self.__acceleration[2] - self.__accelerometer_offset[2])
         
         self.__delta_acceleration = (self.__acceleration[0] - self.__previous_acceleration[0],
                                      self.__acceleration[1] - self.__previous_acceleration[1],
